243f0a1d0ed6ce9cc339276ea6274b1b284f0e06
Diego Batista Pereira <diegobpcelular@gmail.com>
2022-11-05 00:26:03 -0300

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const authenticatedUser = require("../middleware/authentication");
+const testUser = require("../middleware/testUser");
+
+const rateLimiter = require("express-rate-limit");
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: "Too many request from this IP, please try again later",
+  },
+});
+
+const { register, login, updateUser } = require("../controllers/auth");
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
+router.patch("/updateUser", authenticatedUser, testUser, updateUser);
+
+module.exports = router;
